4e1955605eef458d06821d74f71f8d4c05b76601
doe-on <fovusshi01197@gmail.com>
2026-04-03 12:27:23 +0900

    Update 1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int sum_range(int *start, int *end) {
+  int result = 0
+  for(int *p = start; p < end; p++) {
+      int result += *p
+        }
+}
+
+int main() {
+    int data[] = {10, 20, 30 , 40, 50, 60, 70};
+
+    int result = sum_range(&data[1], &data[4]);
+
+    printf("%d\n", result);
+    return 0;
+}
